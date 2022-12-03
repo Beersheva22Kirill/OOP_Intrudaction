1306bf7c3ab36ca788d06722ae0683e540f94981
kirill2884 <kirill2884@gmail.com>
2022-12-03 12:22:27 +0200

    Renamed methods of SquareTriangle

--- src/telran/shapes/SquareTriangle.java
+++ src/telran/shapes/SquareTriangle.java
@@ -18,12 +18,12 @@ public class SquareTriangle extends Square {
 			res[width - 1] = line;
 			if (isLeftDiagonal) {
 				for (int i = width - 2; i > 0; i --) {
-					res[i] = getKatetLeft(offset,i-1);
+					res[i] = getLegtLeft(offset,i-1);
 				}
 				res[0] = " ".repeat(offset) + super.getSymbol();			
 			} else {
 				for (int i = 1; i < super.getWidth() -1; i ++) {
-					res[i] = getKatetRight(offset,i);
+					res[i] = getLegRight(offset,i);
 				}
 				res[0] = " ".repeat(offset + width - 1) + super.getSymbol();
 			}
@@ -31,12 +31,12 @@ public class SquareTriangle extends Square {
 		return res;
 	}
 
-	private String getKatetLeft(int offset, int widht) {
+	private String getLegtLeft(int offset, int widht) {
 		
 		return " ".repeat(offset) + super.getSymbol() + " ".repeat(widht) + super.getSymbol();			
 	}
 	
-	private String getKatetRight(int offset, int width) {
+	private String getLegRight(int offset, int width) {
 		
 		return " ".repeat(offset + getWidth()  - width - 1)  + super.getSymbol() + " ".repeat(width-1) +  super.getSymbol() ;			
 	}
