7c756ef1d75818e10acde00c1f72d6bd50750d96
kirill2884 <kirill2884@gmail.com>
2022-12-22 08:20:27 +0200

    HW BinarySearch and OddComparator

--- src/telran/util/tests/EvenOddComparator.java
+++ src/telran/util/tests/EvenOddComparator.java
@@ -8,23 +8,15 @@ public class EvenOddComparator implements Comparator<Integer>{
 	public int compare(Integer o1, Integer o2) {
 		int res = 0; 
 		
-		if (o1 % 2 == 0 && o2 % 2 == 0) {
-			
+		if (o1 % 2 == 0 && o2 % 2 == 0) {			
 			res = o1 < o2 ? -1 : 1; 
-
-		} else if (o1 % 2 != 0 && o2 % 2 != 0) {
-			
-			res = o1 < o2 ? 1 : -1;
-			
-		} else if (o1 % 2 == 0 && o2 % 2 != 0) {
-			
-			res = -1;
-			
-		} else {
-			
+		} else if (o1 % 2 != 0 && o2 % 2 != 0) {			
+			res = o1 < o2 ? 1 : -1;			
+		} else if (o1 % 2 == 0 && o2 % 2 != 0) {			
+			res = -1;			
+		} else {			
 			res = 1;
-		}
-		
+		}		
 		return res;
 	}
 
