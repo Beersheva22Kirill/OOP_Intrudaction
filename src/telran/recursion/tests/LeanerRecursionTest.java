e2e3cd88c07bf214a8aa0b932ed79f7ce1510ab1
kirill2884 <kirill2884@gmail.com>
2023-01-19 13:55:56 +0200

    Recursion initialization

---
+++ src/telran/recursion/tests/LeanerRecursionTest.java
@@ -0,0 +1,18 @@
+package telran.recursion.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LeanerRecursionTest {
+
+	@Test
+	void fTest() {
+		
+	}
+	
+	void f() {
+		f();
+	}
+
+}
