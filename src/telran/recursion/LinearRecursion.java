d3447c5856cc60929e93420b6727010ba0158b13
kirill2884 <kirill2884@gmail.com>
2023-01-21 13:01:39 +0200

    power (not fix)

--- src/telran/recursion/LinearRecursion.java
+++ src/telran/recursion/LinearRecursion.java
@@ -1,5 +1,96 @@
 package telran.recursion;
 
 public class LinearRecursion {
+	
+	public static long factorial(int num) {
+		long res = 0;
+		if (num < 0) {
+			res = factorial(-num);
+		} else if ( num < 2) {
+			res = 1;
+		} else {
+			res = num * factorial(num - 1);
+		}
+		return res;
+	}
+	
+	public static long power(int num, int pow) {
+		if (pow < 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		long res = 0;
+		if (pow < 1) {
+			res = 1;
+		} else {
+			res = num * power(num, pow - 1);
+		}
+		return res;
+	}
+	
+	public static long powerNew(int num, int pow) {
+		//TODO
+		
+		if (pow < 0) {
+			throw new IllegalArgumentException();
+		}
+		powerNew (num,num,pow,pow);
+		//code cannot cycles and operation * and /
+		return powerNew (num,num,pow,pow);
+	}
+	
+	private static int powerNew(int num,int num2, int pow, int pow2) {
+		int res = 0;
+		
+		if(pow2 > 0) {
+			res = sumNum(num, num2);
+			powerNew(res, num2, pow, pow2 - 1);
+		}
+		return res;
+	}
+
+	private static int sumNum(int num, int pow) {
+		int res = 0;
+		if (pow > 0) {
+			res = num + sumNum(num, pow - 1);
+		}
+		return res;
+	}
+
+	public static long square (int x) {
+		//TODO
+		//not cycles and not operators *, / and not additional methods
+		//no static fields
+		return 0;
+	}
+	
+	public static long summ ( int[] array) {
+		
+		return summ(0, array);
+	}
+	
+	private static long summ(int firstIndex, int[] array) {
+		long res = 0;
+		if (firstIndex != array.length) {
+			res = array[firstIndex] + summ(firstIndex + 1, array);
+		} 
+		return res;
+	}
+	
+	public static void reverse(int[] array) {
+		
+		reverse(0,array.length - 1,array);
+			
+	}
+
+	private static void reverse(int firsIndex, int lastIndex, int[] array) {		
+		if(firsIndex < lastIndex) {
+			int a = array[firsIndex];
+			array[firsIndex] = array[lastIndex];
+			array[lastIndex] = a;
+			reverse(firsIndex + 1, lastIndex - 1, array);
+		}
+	}
+	
 
 }
--- src/telran/recursion/tests/LeanerRecursionTest.java
+++ src/telran/recursion/tests/LeanerRecursionTest.java
@@ -1,6 +1,7 @@
 package telran.recursion.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static telran.recursion.LinearRecursion.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +17,50 @@ class LeanerRecursionTest {
 			f(a - 1);
 		}
 		
+	}
+	
+	@Test
+	void factorialTest() {
+		assertEquals(24, factorial(4));
+		assertEquals(24 * 5 * 6 , factorial(6));
+	}
+	
+	@Test
+	void powTest() {
+		assertEquals(1 , power(3,0));
+		assertEquals(3 , power(3,1));
+		assertEquals(27 , power(3,3));
+		assertEquals(-27 , power(-3,3));
+		assertThrowsExactly(IllegalArgumentException.class, () -> power(5, -3));
+	}
+	
+	@Test
+	void powNewTest() {
+		//assertEquals(1 , powerNew(3,0));
+		//assertEquals(3 , powerNew(3,1));
+		assertEquals(81 , powerNew(3,4));
+		assertEquals(-27 , powerNew(-3,3));
+		assertThrowsExactly(IllegalArgumentException.class, () -> powerNew(5, -3));
+	}
+	
+	@Test 
+	void summTest () {
+		int[] array = {1,2,3,4,5,6};
+		assertEquals(21, summ(array));
+		assertEquals(0, summ(new int[0]) );
+	}
+	
+	@Test
+	void reverseTest() {
+		int[] ar = {1,2,3,4,5,6};
+		int[] expected = {6,5,4,3,2,1};
+		
+		int[] ar1 = {1,2,3,4,5,6,7};
+		int[] expected1 = {7,6,5,4,3,2,1};
+		reverse(ar);
+		reverse(ar1);
+		assertArrayEquals(expected, ar);
+		assertArrayEquals(expected1, ar1);
 	}
 
 }
